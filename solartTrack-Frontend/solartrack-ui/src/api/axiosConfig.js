fa9bd1ec8e377d614024deed081aee9ec7004fa4
Ganesh <ganeshpawar1316@gmail.com>
2026-05-27 00:24:38 +0530

    updated base url for deployment

--- solartTrack-Frontend/solartrack-ui/src/api/axiosConfig.js
+++ solartTrack-Frontend/solartrack-ui/src/api/axiosConfig.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const api=axios.create({
-    //backend working on this url currently
-    baseURL:'http://localhost:8080/api',
-    headers :{
-        'Content-Type': 'application/json'
-    }
-})  
+const api = axios.create({
+  baseURL: 'https://solar-track-backend-ulkp.onrender.com/api',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
 export default api;
